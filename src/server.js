cc7eb9fb0d86c66aceb12328f59befeaa00307af
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-03-27 16:40:14 +0700

    Init code

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const hostname = "localhost";
+const port = 8017;
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World</1> ");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`testing backend, server at ${hostname}:${port}`);
+});
