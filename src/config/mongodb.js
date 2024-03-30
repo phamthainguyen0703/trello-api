0eaa1c6fcee8f6bd4ed5b44bc11863071c989538
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-03-31 00:52:48 +0700

    config connect, get , close to DB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,35 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+const MONGODB_URI =
+  "mongodb+srv://phamthainguyen0703:iOZJe5mEDWHrq8A4@cluster0-phamthainguyen.wjtv4nh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-PhamThaiNguyen";
+const DATABASE_NAME = "trello-phamthainguyen-mern-stack";
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+// khởi tạo 1 đối tượng trelloDatabaseInstance ban đầu là null
+let trelloDatabaseInstance = null;
+
+// khởi tạo 1 đối tượng mongoClientInstance để connect mongoDb
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// connect db
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+};
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance)
+    throw new Error("Must connect to Database first!");
+  return trelloDatabaseInstance;
+};
+
+// đóng kết nối tới database
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close();
+};
--- src/server.js
+++ src/server.js
@@ -1,31 +1,47 @@
+/* eslint-disable no-console */
 import express from "express";
-import { mapOrder } from "~/utils/sorts.js";
+import { CONNECT_DB, GET_DB, CLOSE_DB } from "~/config/mongodb";
+import exitHook from "async-exit-hook";
 
-const app = express();
+const START_SERVER = () => {
+  const app = express();
 
-const hostname = "localhost";
-const port = 8017;
+  const hostname = "localhost";
+  const port = 8017;
 
-app.get("/", (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(
-    mapOrder(
-      [
-        { id: "id-1", name: "One" },
-        { id: "id-2", name: "Two" },
-        { id: "id-3", name: "Three" },
-        { id: "id-4", name: "Four" },
-        { id: "id-5", name: "Five" },
-      ],
-      ["id-5", "id-4", "id-2", "id-3", "id-1"],
-      "id"
-    )
-  );
-  res.end("<h1>Hello World!</h1><hr>");
-});
+  app.get("/", (req, res) => {
+    res.end("<h1>Hello World!</h1><hr>");
+  });
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`);
-});
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`running at http://${hostname}:${port}/`);
+  });
+
+  // thực hiện các tác vụ clean up trước khi dừng server
+
+  exitHook(() => {
+    CLOSE_DB();
+    console.log("Exiting with signal");
+  });
+};
+
+// immediately-invoked / anonymous async functions (IIFE)
+(async () => {
+  try {
+    await CONNECT_DB();
+    console.log("Connected to MongoDB Cloud Atlas!!");
+    START_SERVER();
+  } catch (err) {
+    console.log(err);
+    process.exit(0);
+  }
+})();
+
+// CONNECT_DB()
+//   .then(console.log("Connected to MongoDB Cloud Atlas!!"))
+//   .then(() => START_SERVER())
+//   .catch((err) => {
+//     console.log(err);
+//     process.exit(0);
+//   });
