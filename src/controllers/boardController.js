f377695c73b863dcae9379eec86e4b5db60b0a69
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-04-02 15:48:00 +0700

    Viết API Get Board Details

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -17,6 +17,20 @@ const createNew = async (req, res, next) => {
   }
 };
 
+const getDetails = async (req, res, next) => {
+  try {
+    const boardId = req.params.id;
+
+    const board = await boardService.getDetails(boardId);
+    res.status(StatusCodes.OK).json(board);
+  } catch (error) {
+    next(
+      new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message)
+    );
+  }
+};
+
 export const boardController = {
   createNew,
+  getDetails,
 };
