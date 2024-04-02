ec06dadfe74d63d011035eb5a408ec3bc5cf42a9
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-04-02 15:00:08 +0700

    xử lí vấn đề insert data không có dữ liệu mặc định bằng cách validate cả  ở Validation và Model Schema

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
-import { Collection } from "mongodb";
+import { ObjectId } from "mongodb";
 import { GET_DB } from "~/config/mongodb";
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
 
@@ -18,11 +18,18 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false),
 });
 
+const validationBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false,
+  });
+};
+
 const createNew = async (data) => {
   try {
+    const validData = await validationBeforeCreate(data);
     const createdBoard = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .insertOne(data);
+      .insertOne(validData);
     return createdBoard;
   } catch (error) {
     throw new Error(error);
@@ -33,7 +40,7 @@ const findOneById = async (id) => {
   try {
     const result = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .findOne({ _id: id });
+      .findOne({ _id: new ObjectId(id) });
     return result;
   } catch (error) {
     throw new Error(error);
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -9,11 +9,9 @@ const createNew = async (reqBody) => {
     };
     // gọi tới modle để handle lưu bản ghi newBoard vào trong database
     const createdBoard = await boardModel.createNew(newBoard);
-    console.log("createdBoard", createdBoard);
 
     // lấy bản ghi board
     const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
-    console.log(getNewBoard);
     return getNewBoard;
   } catch (error) {
     throw error;
