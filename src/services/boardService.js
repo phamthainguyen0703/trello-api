6d003aa30a59aefd1b164dbe3f382b379b88c808
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-04-02 13:34:25 +0700

    config boardModel

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-useless-catch */
+import { boardModel } from "~/models/boardModel";
 import { slugify } from "~/utils/formatters";
 const createNew = async (reqBody) => {
   try {
@@ -6,8 +7,14 @@ const createNew = async (reqBody) => {
       ...reqBody,
       slug: slugify(reqBody.title),
     };
+    // gọi tới modle để handle lưu bản ghi newBoard vào trong database
+    const createdBoard = await boardModel.createNew(newBoard);
+    console.log("createdBoard", createdBoard);
 
-    return newBoard;
+    // lấy bản ghi board
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
+    console.log(getNewBoard);
+    return getNewBoard;
   } catch (error) {
     throw error;
   }
