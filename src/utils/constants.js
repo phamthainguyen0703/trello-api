c95a893eaad32ef1d24b0bc349a92b32bcf746bd
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2024-04-11 17:37:01 +0700

    update CORS not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 // những domain được phép truy cập tới tài nguyên của server
-export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+export const WHITELIST_DOMAINS = [
+  // "http://localhost:5173"
+];
 
 export const BOARD_TYPE = {
   PUBLIC: "public",
